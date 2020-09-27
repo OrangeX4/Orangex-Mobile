13c99ae18e883883ec7ae1eb9712adc0c6167554
OrangeX4 <318483724@qq.com>
2020-09-27 20:30:22 +0800

    Refactor with React

---
+++ public/precache-manifest.7ed42b0bbc6bb349e7243ae6bd26f83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330350897ba3d26569b56ec9c0c5b1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f44a32c8af84808c07f",
+    "url": "/static/css/main.0f933960.chunk.css"
+  },
+  {
+    "revision": "6fb547919c4046cc3e0b",
+    "url": "/static/js/2.e1421900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1421900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f44a32c8af84808c07f",
+    "url": "/static/js/main.0100f7e0.chunk.js"
+  },
+  {
+    "revision": "522fe2bb7f3e39ed9fd7",
+    "url": "/static/js/runtime-main.fea2a137.js"
+  }
+]);
